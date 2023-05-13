52f4d9c77246fb6593fc1b82196bd7bcde2870c4
--add <tarun@iotric.com>
2023-05-13 19:26:30 +0530

    addd charts

--- backend/controllers/client.js
+++ backend/controllers/client.js
@@ -49,6 +49,7 @@ export const getTransactions = async (req, res) => {
 
             return sortFormatted;
         };
+
         const sortFormatted = Boolean(sort) ? generateSort() : {};
 
         const transactions = await Transaction.find({
--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,8 @@ import morgan from "morgan";
 // routes
 import clientRoutes from "./routes/client.js";
 import generalRoutes from "./routes/general.js";
-// import managementRoutes from "./routes/management.js";
-// import salesRoutes from "./routes/sales.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
 // data imports
 import User from "./models/User.js";
@@ -43,8 +43,8 @@ app.use(cors());
 /* ROUTES */
 app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
-// app.use("/management", managementRoutes);
-// app.use("/sales", salesRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
